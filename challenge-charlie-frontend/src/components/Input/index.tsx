becfd87fabd57eea92b97f4d85af5eefcb0a5b30
Iuri Souza <iuri.santanna@tvglobo.com.br>
2020-07-19 05:31:46 -0300

    feat: input layout component

--- challenge-charlie-frontend/src/components/Input/index.tsx
+++ challenge-charlie-frontend/src/components/Input/index.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import TextField from "@material-ui/core/TextField";
+import ExploreOutlinedIcon from "@material-ui/icons/ExploreOutlined";
+import "./styles.scss";
+import { Box } from "@material-ui/core";
 
 export default function Input() {
-    return <input type="text" />;
+    return (
+        <Box
+            display="flex"
+            id="input-box"
+            p={3}
+            alignContent="center"
+            style={{ background: "rgba(255,255,255,0.6)" }}
+        >
+            <Box mr={2}>
+                <ExploreOutlinedIcon
+                    style={{ width: "60px", height: "60px" }}
+                />
+            </Box>
+            <Box flexGrow={1}>
+                <TextField id="input-local" fullWidth />
+            </Box>
+        </Box>
+    );
 }
