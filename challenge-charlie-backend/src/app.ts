cafe7e60acc324e551fbed1d73dea4abd08b115d
Iuri Souza <iuri.santanna@tvglobo.com.br>
2020-07-19 02:32:09 -0300

    feat: add cors

--- challenge-charlie-backend/src/app.ts
+++ challenge-charlie-backend/src/app.ts
@@ -1,11 +1,16 @@
 import express from "express";
+import cors from "cors";
 import images from "./routes/images";
 import weather from "./routes/weather";
 
 const app = express();
+const port = 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use(images);
 app.use(weather);
 
-app.listen(3333);
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
